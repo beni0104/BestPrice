598dd61af1eed357e04bc71074e1c797c8b69c90
beni <szekrenyes.beni@gmail.com>
2024-01-22 12:28:56 +0200

    file cleanup

--- backend/scraper/scraper/spiders/gsmarenaspider.py
+++ backend/scraper/scraper/spiders/gsmarenaspider.py
@@ -6,7 +6,7 @@ class GSMArenaSpider(scrapy.Spider):
     # allowed_domains = ['gsmarena.com']
 
     custom_settings = {
-        'DOWNLOAD_DELAY': 10
+        'DOWNLOAD_DELAY': 5
     }
 
     def start_requests(self):
